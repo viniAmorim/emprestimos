3d5aca9a41cc58daf3fb270f422794b9a55a2b2c
viniAmorim <amorimm.viniciuss@gmail.com>
2025-06-02 13:28:50 -0300

    correction on list clients-> show data-> next due date

--- app/public_html/painel/paginas/clientes/listar_cobrancas.php
+++ app/public_html/painel/paginas/clientes/listar_cobrancas.php
@@ -34,7 +34,7 @@ $data_venc = $res[$i]['data_venc'];
 $data = $res[$i]['data'];
 
 $data_vencF = date('d', strtotime($data_venc));
-$dataF = implode('/', array_reverse(explode('-', $data)));
+$dataF = implode('/', array_reverse(explode('-', $data ?? '')));
 $valorF = number_format($valor, 2, ',', '.');
 
 $classe_deb = '';
@@ -66,7 +66,7 @@ if($parcelas > 1){
 $query2 = $pdo->query("SELECT * FROM receber where referencia = 'Cobrança' and id_ref = '$id_emp' and pago != 'Sim' order by id asc limit 1");
 $res2 = $query2->fetchAll(PDO::FETCH_ASSOC);
 $data_ultimo_venc = @$res2[0]['data_venc'];
-$data_ultimo_vencF = implode('/', array_reverse(explode('-', $data_ultimo_venc)));
+$data_ultimo_vencF = implode('/', array_reverse(explode('-', $data_ultimo_venc ?? '')));
 
 
 echo <<<HTML
