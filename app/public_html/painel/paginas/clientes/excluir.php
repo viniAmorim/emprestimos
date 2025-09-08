88e8d152a8d51bfe3f650b673d2376092ba3a33c
viniAmorim <amorimm.viniciuss@gmail.com>
2025-09-08 14:35:42 -0300

    fix delete user on users table

--- app/public_html/painel/paginas/clientes/excluir.php
+++ app/public_html/painel/paginas/clientes/excluir.php
@@ -1,28 +1,47 @@
 <?php 
 $tabela = 'clientes';
+
 require_once("../../../conexao.php");
 
 $id = $_POST['id'];
 
-$query = $pdo->query("SELECT * FROM $tabela where id = '$id'");
-$res = $query->fetchAll(PDO::FETCH_ASSOC);
-$total_reg = @count($res);
-$comprovante_rg = $res[0]['comprovante_rg'];
-$comprovante_endereco = $res[0]['comprovante_endereco'];
-$foto = $res[0]['foto'];
+try {
+    $pdo->beginTransaction();
 
-if($comprovante_rg != "sem-foto.png"){
-	@unlink('../../images/comprovantes/'.$comprovante_rg);
+    $pdo->query("DELETE FROM alertas_duplicidade WHERE id_cliente_cadastrado = '$id'");
+
+    $query_cliente = $pdo->query("SELECT * FROM $tabela WHERE id = '$id'");
+    $res = $query_cliente->fetchAll(PDO::FETCH_ASSOC);
+    $total_reg = @count($res);
+
+    if ($total_reg > 0) {
+        $comprovante_rg = $res[0]['comprovante_rg'];
+        $comprovante_endereco = $res[0]['comprovante_endereco'];
+        $foto = $res[0]['foto'];
+
+        if($comprovante_rg != "sem-foto.png"){
+            @unlink('../../images/comprovantes/'.$comprovante_rg);
+        }
+
+        if($comprovante_endereco != "sem-foto.png"){
+            @unlink('../../images/comprovantes/'.$comprovante_endereco);
+        }
+
+        if($foto != "sem-foto.jpg"){
+            @unlink('../../images/clientes/'.$foto);
+        }
+    }
+   
+    $pdo->query("DELETE FROM $tabela WHERE id = '$id' ");
+
+    
+    $pdo->commit();
+
+    echo 'Excluído com Sucesso';
+
+} catch (PDOException $e) {
+    $pdo->rollBack();
+  
+    echo 'Erro ao excluir: ' . $e->getMessage();
 }
-
-if($comprovante_endereco != "sem-foto.png"){
-	@unlink('../../images/comprovantes/'.$comprovante_endereco);
-}
-
-if($foto != "sem-foto.jpg"){
-	@unlink('../../images/clientes/'.$foto);
-}
-
-$pdo->query("DELETE FROM $tabela WHERE id = '$id' ");
-echo 'Excluído com Sucesso';
 ?>
